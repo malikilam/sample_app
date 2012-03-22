aa7c1ec0dfecd15c6e4be0d4046896cc7eb2043c
Malik Miller <malikilam@hotmail.com>
2012-03-22 13:32:56 -0400

    finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -14,17 +15,54 @@ class User < ActiveRecord::Base
   
   attr_accessible :name, :email, :password, :password_confirmation
   
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence => true,
             :length => { :maximum => 50 }
             
-  validates :email, :presence => true,
+  validates :email, :presence => true ,
             :format => { :with => email_regex },
             :uniqueness => { :case_sensitive => false }
             
   # Automatically create the virtual attribute 'password_confirmation'.
   validates :password, :presence => true,
-                        :confirmation => true,
+                        :confirmation => true ,
                         :length => { :within => 6..40 }
+                        
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+ 
+  class << self
+    def authenticate(email, submitted_password)
+      user = User.find_by_email(email)
+      return nil  if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+  end
+    
+  private
+    
+    def encrypt_password
+      self.salt = make_salt if self.new_record?
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)      
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+   
 end
+
+
+
